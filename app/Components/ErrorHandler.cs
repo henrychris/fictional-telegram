6c7643a1cfba5833a80baa0a8e3d7c44c65970ef
henrychris <henrychris2003@gmail.com>
2021-07-27 11:21:39 +0100

    create global error handler

---
+++ app/Components/ErrorHandler.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading.Tasks;
+using Contracts;
+using Telegram.Bot.Exceptions;
+
+namespace app.Components
+{
+    public class ErrorHandler
+    {
+        private readonly ILoggerManager _logger;
+        public ErrorHandler(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        public Task HandleErrorAsync(Exception exception)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            _logger.LogError(ErrorMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
