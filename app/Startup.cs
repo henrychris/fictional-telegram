2d60bf838efa5bc5c5d4c32e4a84ea73f3366e6c
henrychris <henrychris2003@gmail.com>
2021-07-27 11:20:54 +0100

    Organised code, visually.

--- app/Startup.cs
+++ app/Startup.cs
@@ -1,57 +1,49 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using app.Services;
 using Contracts;
 using LoggerService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using NLog;
 using Telegram.Bot;
+using app.Components;
+using app.Services;
 
 namespace app
 {
+
+    public static class ServicesExtensions
+    {
+
+        public static void AddComponents(this IServiceCollection services)
+        {
+            services.AddTransient<Keyboards>();
+            services.AddScoped<ErrorHandler>();
+        }
+
+        public static void ConfigureAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(Startup));
+        }
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+    }
     public class Startup
     {
-        protected BotConfiguration BotConfig { get; }
-        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
             LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
             BotConfig = Configuration.GetSection("BotConfiguration").Get<BotConfiguration>();
         }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Telegram.Bot uses JSON.NET to deserialize incoming messages.
-            services.AddControllers().AddNewtonsoftJson();
-
-            services.AddHostedService<ConfigureWebhook>();
-
-            
-            services.AddScoped<HandleUpdateService>();
-
-            services.AddHttpClient("tgwebhook")
-                    .AddTypedClient<ITelegramBotClient>(httpClient
-                        => new TelegramBotClient(BotConfig.BotToken, httpClient));
-
-            services.ConfigureLoggerService();
-            services.ConfigureAutoMapper();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "app", Version = "v1" });
-            });
-        }
+        public IConfiguration Configuration { get; }
+        protected BotConfiguration BotConfig { get; }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -81,18 +73,29 @@ namespace app
                                             new { controller = "Webhook", action = "Post" });
             });
         }
-    }
 
-    public static class ServicesExtensions
-    {
-        public static void ConfigureLoggerService(this IServiceCollection services)
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<ILoggerManager, LoggerManager>();
-        }
+            // Telegram.Bot uses JSON.NET to deserialize incoming messages.
+            services.AddControllers().AddNewtonsoftJson();
 
-        public static void ConfigureAutoMapper(this IServiceCollection services)
-        {
-            services.AddAutoMapper(typeof(Startup));
+            services.AddHostedService<ConfigureWebhook>();
+
+            services.AddScoped<HandleUpdateService>();
+            services.AddTransient<UserActionsService>();
+
+            services.AddHttpClient("tgwebhook")
+                    .AddTypedClient<ITelegramBotClient>(httpClient
+                        => new TelegramBotClient(BotConfig.BotToken, httpClient));
+
+            services.ConfigureLoggerService();
+            services.ConfigureAutoMapper();
+            services.AddComponents();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "app", Version = "v1" });
+            });
         }
     }
 }
