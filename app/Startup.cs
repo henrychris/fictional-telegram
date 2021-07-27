cedc4c8460f6c3f88b7f3406676ec2eb16edc031
henrychris <henrychris2003@gmail.com>
2021-07-27 09:30:37 +0100

    add webhook client and route

--- app/Startup.cs
+++ app/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using app.Services;
 using Contracts;
 using LoggerService;
 using Microsoft.AspNetCore.Builder;
@@ -14,12 +15,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using NLog;
+using Telegram.Bot;
 
 namespace app
 {
     public class Startup
     {
         protected BotConfiguration BotConfig { get; }
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,12 +30,21 @@ namespace app
             BotConfig = Configuration.GetSection("BotConfiguration").Get<BotConfiguration>();
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            // Telegram.Bot uses JSON.NET to deserialize incoming messages.
+            services.AddControllers().AddNewtonsoftJson();
+
+            services.AddHostedService<ConfigureWebhook>();
+
+            
+            services.AddScoped<HandleUpdateService>();
+
+            services.AddHttpClient("tgwebhook")
+                    .AddTypedClient<ITelegramBotClient>(httpClient
+                        => new TelegramBotClient(BotConfig.BotToken, httpClient));
+
             services.ConfigureLoggerService();
             services.ConfigureAutoMapper();
             services.AddSwaggerGen(c =>
@@ -61,7 +73,12 @@ namespace app
 
             app.UseEndpoints(endpoints =>
             {
+                var token = BotConfig.BotToken;
                 endpoints.MapControllers();
+
+                endpoints.MapControllerRoute(name: "tgwebhook",
+                                            pattern: $"bot/{token}",
+                                            new { controller = "Webhook", action = "Post" });
             });
         }
     }
