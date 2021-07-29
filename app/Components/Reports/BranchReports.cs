f661fba5b1fc9d2a30284a8943c6fb086c28ba5d
henrychris <henrychris2003@gmail.com>
2021-07-29 11:37:53 +0100

    add shell for sending reports.

--- app/Components/Reports/BranchReports.cs
+++ app/Components/Reports/BranchReports.cs
@@ -12,9 +12,47 @@ namespace app.Components.Reports
             _botClient = botClient;
         }
 
+        // TODO
+        /*
+            receive callback data and run it through a switch to get the time frame for report.
+            ! figure out how to store user company Ids.
+            ? send typing action while pdf processes
+        */
+
         public async Task<Message> SendBranchCashflowReportAsync(CallbackQuery query, Message message)
         {
-            return await _botClient.SendTextMessageAsync(message.Chat.Id, "Testing");
+            // delete the message
+            await _botClient.DeleteMessageAsync(query.Message.Chat.Id, query.Message.MessageId);
+            return await _botClient.SendTextMessageAsync(message.Chat.Id, $"Branch Cashflow: {query.Data}");
+        }
+
+        public async Task<Message> SendProductSummaryReportAsync(CallbackQuery query, Message message)
+        {
+            await _botClient.DeleteMessageAsync(query.Message.Chat.Id, query.Message.MessageId);
+            return await _botClient.SendTextMessageAsync(message.Chat.Id, $"Product Summary: {query.Data}");
+        }
+
+        public async Task<Message> SendBranchSalesTransactionsReportAsync(CallbackQuery query, Message message)
+        {
+            await _botClient.DeleteMessageAsync(query.Message.Chat.Id, query.Message.MessageId);
+            return await _botClient.SendTextMessageAsync(message.Chat.Id, $"Branch Sales Transactions: {query.Data}");
+        }
+
+        public async Task<Message> SendBranchTanksFilledReportAsync(CallbackQuery query, Message message)
+        {
+            await _botClient.DeleteMessageAsync(query.Message.Chat.Id, query.Message.MessageId);
+            return await _botClient.SendTextMessageAsync(message.Chat.Id, $"Branch Tanks Filled: {query.Data}");
+        }
+
+        public async Task<Message> SendBranchVarianceReportAsync(CallbackQuery query, Message message)
+        {
+            await _botClient.DeleteMessageAsync(query.Message.Chat.Id, query.Message.MessageId);
+            return await _botClient.SendTextMessageAsync(message.Chat.Id, $"Branch Variance: {query.Data}");
+        }
+
+        public async Task<Message> SendBranchTankReporAsync(CallbackQuery query, Message message)
+        {
+            return await _botClient.SendTextMessageAsync(message.Chat.Id, $"Branch Tank Report: {query.Data}");
         }
     }
 }
--- app/Components/Reports/BranchReportsKeyboards.cs
+++ app/Components/Reports/BranchReportsKeyboards.cs
@@ -10,17 +10,8 @@ namespace app.Components.Reports
     {
         private readonly ITelegramBotClient _botClient;
         private readonly IUserRepository _userRepository;
-        public BranchReportsKeyboards(ITelegramBotClient botClient, IUserRepository userRepository)
-        {
-            _userRepository = userRepository;
-            _botClient = botClient;
-        }
-        public async Task SendBranchCashflowReportKeyboard(Message message)
-        {
-            // delete last message
-            await _botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId);
 
-            var keyboard = new InlineKeyboardMarkup(new[]
+        InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup(new[]
             {
             new[]
             {
@@ -38,30 +29,63 @@ namespace app.Components.Reports
             }
             });
 
+        public BranchReportsKeyboards(ITelegramBotClient botClient, IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+            _botClient = botClient;
+        }
+        public async Task SendBranchCashflowReportKeyboard(Message message)
+        {
+            // delete last message
+            await _botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+
             await _botClient.SendTextMessageAsync(message.Chat.Id, "Select a time range for the cashflow report", replyMarkup: keyboard);
             await _userRepository.SetUserStateAsync(message.Chat.Id, "BranchCashflowReport");
-    
         }
 
-        // public void SetReportRangeKeyboard(Message message)
-        // {
-        //     var keyboard = new InlineKeyboardMarkup(new[]
-        //     {
-        //     new[]
-        //     {
-        //         InlineKeyboardButton.WithCallbackData("Today"),
-        //         InlineKeyboardButton.WithCallbackData("Yesterday")
-        //     },
-        //     new[]
-        //     {
-        //         InlineKeyboardButton.WithCallbackData("Last 7 Days", "Week"),
-        //         InlineKeyboardButton.WithCallbackData("Last 30 Days", "Month")
-        //     },
-        //     new[]
-        //     {
-        //         InlineKeyboardButton.WithCallbackData("Back", "Reports")
-        //     }
-        //     });
-        // }
+        public async Task SendProductSummaryReportKeyboard(Message message)
+        {
+            // delete last message
+            await _botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+
+            await _botClient.SendTextMessageAsync(message.Chat.Id, "Select a time range for the Product Summary report", replyMarkup: keyboard);
+            await _userRepository.SetUserStateAsync(message.Chat.Id, "BranchProductSummmaryReport");
+        }
+
+        public async Task SendBranchSalesTransactionsReportKeyboard(Message message)
+        {
+            // delete last message
+            await _botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+
+            await _botClient.SendTextMessageAsync(message.Chat.Id, "Select a time range for the Sales Transactions report", replyMarkup: keyboard);
+            await _userRepository.SetUserStateAsync(message.Chat.Id, "BranchSalesTransactionsReport");
+        }
+
+        public async Task SendBranchTanksFilledReportKeyboard(Message message)
+        {
+            // delete last message
+            await _botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+
+            await _botClient.SendTextMessageAsync(message.Chat.Id, "Select a time range for the Tanks Filled report", replyMarkup: keyboard);
+            await _userRepository.SetUserStateAsync(message.Chat.Id, "BranchTanksFilledReport");
+        }
+
+        public async Task SendBranchVarianceReportKeyboard(Message message)
+        {
+            // delete last message
+            await _botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+            
+            await _botClient.SendTextMessageAsync(message.Chat.Id, "Select a time range for the Variance report", replyMarkup: keyboard);
+            await _userRepository.SetUserStateAsync(message.Chat.Id, "BranchVarianceReport");
+        }
+
+        public async Task SendBranchTankReportKeyboard(Message message)
+        {
+            // delete last message
+            await _botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+
+            await _botClient.SendTextMessageAsync(message.Chat.Id, "Select a time range for the Tank report", replyMarkup: keyboard);
+            await _userRepository.SetUserStateAsync(message.Chat.Id, "BranchTankReport");
+        }
     }
 }
