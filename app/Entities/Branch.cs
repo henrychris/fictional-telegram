5612e64c865720d8203a214ce0bc5c87b7a9742f
henrychris <henrychris2003@gmail.com>
2021-08-03 12:37:18 +0100

    add CompanyId FK

--- app/Entities/Branch.cs
+++ app/Entities/Branch.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace app.Entities
 {
@@ -7,5 +8,9 @@ namespace app.Entities
         public string Id { get; set; }
         public string CompanyName { get; set; }
         public string Name { get; set; }
+
+        [ForeignKey("CompanyId")]
+        public string CompanyId { get; set; }
+        public virtual Company Company { get; set; }
     }
 }
