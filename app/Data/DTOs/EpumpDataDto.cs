391b5a0416ab184fc95452d54cf3525de5e52a49
henrychris <henrychris2003@gmail.com>
2021-08-19 08:52:41 +0100

    add profile and dto for mapping epumpData

---
+++ app/Data/DTOs/EpumpDataDto.cs
@@ -0,0 +1,10 @@
+namespace app.Data.DTOs
+{
+    public class EpumpDataDto
+    {
+        public long ChatId { get; set; }
+        public string CompanyId { get; set; }
+        public string EpumpId { get; set; }
+        public string AuthKey { get; set; }
+    }
+}
