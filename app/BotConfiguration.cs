49cb70efffd19d1c5584e3cf4e003f23cda8638a
henrychris <henrychris2003@gmail.com>
2021-08-19 10:28:23 +0100

    add new Uri's to config

--- app/BotConfiguration.cs
+++ app/BotConfiguration.cs
@@ -5,7 +5,8 @@ namespace app
         public string BotToken { get; init; }
         public string WebhookUrl { get; init; }
         public string DefaultConnection { get; init; }
-        public string LoginUrl { get; init; }
+        public string TelegramLoginUrl { get; init; }
+        public string EpumpLoginUrl { get; init; }
         public string EpumpReportUri { get; init; }
         public string EpumpApiUri { get; init; }
 
