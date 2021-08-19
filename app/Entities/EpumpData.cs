a77c64ad773b92ee9d411af11366a67b72c18044
henrychris <henrychris2003@gmail.com>
2021-08-19 14:14:34 +0100

    removed company table

--- app/Entities/EpumpData.cs
+++ app/Entities/EpumpData.cs
@@ -4,10 +4,9 @@ namespace app.Entities
     {
         public string ID { get; set; }
         public long ChatId { get; set; } // foreign key
-        public string CompanyId { get; set; } // foreign key
+        public string CompanyId { get; set; }
         public string AuthKey { get; set; }
         public string Role { get; set; }
         public virtual AppUser User { get; set; }
-        public virtual Company Company { get; set; }
     }
 }
