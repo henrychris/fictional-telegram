8336f021bc213f140fc71f5e4d5ced4d8910f786
henrychris <henrychris2003@gmail.com>
2021-08-27 14:39:39 +0100

    changed formatting

--- app/Middleware/ExceptionMiddleware.cs
+++ app/Middleware/ExceptionMiddleware.cs
@@ -25,20 +25,20 @@ namespace app.Middleware
         public async Task InvokeAsync(HttpContext context)
         {
             try
-            { 
+            {
                 await _next(context);
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, $"Error in ExceptionMiddleware: {ex.Message}");
                 context.Response.ContentType = "application/json";
-                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
                 var response = _env.IsDevelopment()
                 ? new ErrorDetails(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
                 : new ErrorDetails(context.Response.StatusCode, "Internal Server Error");
 
-                var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
                 var json = JsonSerializer.Serialize(response, options);
 
                 await context.Response.WriteAsync(json);
