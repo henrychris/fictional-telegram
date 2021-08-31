b912c57bc24a58681f2e379131aef9353bcbb4d4
henrychris <henrychris2003@gmail.com>
2021-08-31 14:16:09 +0100

    fixed disposed context error

--- app/Data/Repository/LoginStatusMethods.cs
+++ app/Data/Repository/LoginStatusMethods.cs
@@ -17,46 +17,53 @@ namespace app.Data.Repository
 
         public async Task AddAsync(LoginStatusTelegram user)
         {
-            await _context.loginStatusTelegram.AddAsync(user);
-            await _context.SaveChangesAsync();
+            await using var context = new DataContext();
+            await context.loginStatusTelegram.AddAsync(user);
+            await context.SaveChangesAsync();
         }
 
         public async Task AddAsync(LoginStatusEpump user)
         {
-            await _context.loginStatusEpump.AddAsync(user);
-            await _context.SaveChangesAsync();
+            await using var context = new DataContext();
+            await context.loginStatusEpump.AddAsync(user);
+            await context.SaveChangesAsync();
         }
 
         public async Task<bool> IsUserLoggedInAsync(long chatId, string epumpId)
         {
-            var telegramUser = await _context.loginStatusTelegram.AsNoTracking().AnyAsync(u => u.UserChatId == chatId);
-            var epumpUser = await _context.loginStatusEpump.AsNoTracking().AnyAsync(u => u.EpumpDataId == epumpId);
+            await using var context = new DataContext();
+            var telegramUser = await context.loginStatusTelegram.AsNoTracking().AnyAsync(u => u.UserChatId == chatId);
+            var epumpUser = await context.loginStatusEpump.AsNoTracking().AnyAsync(u => u.EpumpDataId == epumpId);
 
             return telegramUser && epumpUser;
         }
 
         public async Task<bool> IsUserLoggedInAsync_Epump(string id)
         {
-            return await _context.loginStatusEpump.AnyAsync(u => u.EpumpDataId == id);
+            await using var context = new DataContext();
+            return await context.loginStatusEpump.AnyAsync(u => u.EpumpDataId == id);
         }
 
         public async Task<bool> IsUserLoggedInAsync_Telegram(long chatId)
         {
-            return await _context.loginStatusTelegram.AnyAsync(u => u.UserChatId == chatId);
+            await using var context = new DataContext();
+            return await context.loginStatusTelegram.AnyAsync(u => u.UserChatId == chatId);
         }
 
         public async Task SetEpumpLoginStatusAsync(string epumpId, DateTime dateTime, bool status)
         {
-            var epumpUser = await _context.loginStatusEpump.FirstOrDefaultAsync(u => u.EpumpDataId == epumpId);
+            await using var context = new DataContext();
+            var epumpUser = await context.loginStatusEpump.FirstOrDefaultAsync(u => u.EpumpDataId == epumpId);
             epumpUser.IsLoggedIn = status;
-            await _context.SaveChangesAsync();
+            await context.SaveChangesAsync();
         }
 
         public async Task SetTelegramLoginStatusAsync(long chatId, DateTime dateTime, bool status)
         {
-            var telegramUser = await _context.loginStatusTelegram.FirstOrDefaultAsync(u => u.UserChatId == chatId);
+            await using var context = new DataContext();
+            var telegramUser = await context.loginStatusTelegram.FirstOrDefaultAsync(u => u.UserChatId == chatId);
             telegramUser.IsLoggedIn = status;
-            await _context.SaveChangesAsync();
+            await context.SaveChangesAsync();
         }
     }
 }
