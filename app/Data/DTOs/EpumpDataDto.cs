2c551358d40860fec03f54081c0a4fbf89cac442
henrychris <henrychris2003@gmail.com>
2021-09-09 15:19:38 +0100

    add new Dto to handle OTP authorization

--- app/Data/DTOs/EpumpDataDto.cs
+++ app/Data/DTOs/EpumpDataDto.cs
@@ -2,9 +2,7 @@ namespace app.Data.DTOs
 {
     public class EpumpDataDto
     {
-        public long ChatId { get; set; }
-        public string CompanyId { get; set; }
-        public string EpumpId { get; set; }
-        public string AuthKey { get; set; }
+        public EpumpUser user { get; set; }
+        public string token { get; set; }
     }
 }
