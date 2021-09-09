380639969ec1dccfff0b6a3d0bc76a781a561011
henrychris <henrychris2003@gmail.com>
2021-09-09 14:47:50 +0100

    moved logic to helper class

--- app/Controllers/BotController.cs
+++ app/Controllers/BotController.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Threading.Tasks;
+using app.Controllers.Helper;
 using app.Data.DTOs;
-using app.Entities;
-using app.Interfaces;
-using AutoMapper;
 using Contracts;
 using Microsoft.AspNetCore.Mvc;
 using Telegram.Bot;
@@ -15,81 +13,24 @@ namespace app.Controllers
     [Route("/api/[controller]")]
     public class BotController : ControllerBase
     {
-        private readonly IMapper _mapper;
         private readonly ILoggerManager _logger;
-        private readonly IUserRepository _userRepository;
-        private readonly IEpumpDataRepository _epumpDataRepository;
         private readonly ITelegramBotClient _botClient;
-        private readonly ILoginStatusRepository _loginStatusRepository;
+        private readonly BotControllerHelper _botControllerHelper;
 
-        public BotController(IMapper mapper, ILoggerManager logger, IUserRepository userRepository, IEpumpDataRepository epumpDataRepository,
-        ITelegramBotClient botClient, ILoginStatusRepository loginStatusRepository)
+        public BotController(ILoggerManager logger,
+        ITelegramBotClient botClient, BotControllerHelper botControllerHelper)
         {
-            _loginStatusRepository = loginStatusRepository;
+            _botControllerHelper = botControllerHelper;
             _botClient = botClient;
-            _epumpDataRepository = epumpDataRepository;
-            _userRepository = userRepository;
             _logger = logger;
-            _mapper = mapper;
         }
 
         [HttpGet("register")]
-        public async Task<ActionResult<TelegramUserDto>> RegisterUser([FromQuery] TelegramUserDto data)
+        public async Task<ActionResult> RegisterUser([FromQuery] TelegramUserDto data)
         {
-            if (data == null) return BadRequest("No data");
-
-            var user = _mapper.Map<AppUser>(data);
-
-            if (await _loginStatusRepository.IsUserLoggedInAsync_Telegram(user.ChatId))
-            {
-                return BadRequest("User already exists");
-            }
-            else
-            {
-                await _userRepository.AddUserAsync(user);
-                await _loginStatusRepository.AddAsync(new LoginStatusTelegram
-                {
-                    UserChatId = user.ChatId,
-                    LoginDate = DateTime.Now,
-                    IsLoggedIn = true
-                });
-
-                _logger.LogInfo($"User {user.FirstName} with ID {user.ChatId} has been added to the database, at {DateTime.Now}");
-                // redirects user to epump login page
-                return Redirect("https://epump-login-test.herokuapp.com/");
-            }
+            return await _botControllerHelper.RegisterUser(data);
         }
 
-        [HttpPost("register/epump")]
-        public async Task<ActionResult> RegisterEpumpUser(EpumpDataDto data)
-        {
-            // If the epump Id(PK) is missing, it can't be added to the db anyway.
-            if (data == null || data.EpumpId == null) return BadRequest("No data");
-
-            var user = _mapper.Map<EpumpData>(data);
-            var check = await _loginStatusRepository.IsUserLoggedInAsync(user.ChatId, user.ID);
-            if (check)
-            {
-                return BadRequest("User already exists");
-            }
-            else
-            {
-                await _epumpDataRepository.AddUserAsync(user);
-                _logger.LogInfo($"User {user.ChatId}, EpumpID: {user.ID} registered.");
-
-                await _userRepository.FindAndUpdateUserWithEpumpDataAsync(user.ChatId, user.ID);
-                _logger.LogInfo($"User {user.ChatId} database entry updated with EpumpID: {user.ID}");
-
-                await _loginStatusRepository.AddAsync(new LoginStatusEpump
-                {
-                    EpumpDataId = user.ID,
-                    LoginDate = DateTime.Now,
-                    IsLoggedIn = true
-                });
-                _logger.LogInfo($"User {user.ID} has been fully added to the database, at {DateTime.Now}");
-                return StatusCode(201);
-            }
-        }
 
         [HttpPost("notification")]
         public async Task<ActionResult> SendUserNotification([FromBody] NotificationDto data)
@@ -100,8 +41,3 @@ namespace app.Controllers
         }
     }
 }
-/*
-    Regarding Authorization:
-    I wish to use an OTP on the epump site to authorize the user, before updating the data.
-    So, more work to do.
-*/
