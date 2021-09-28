ed570a491472ea5af22172520edbbbe3ef2a6658
henrychris <henrychris2003@gmail.com>
2021-09-28 09:59:18 +0100

    Revert "add required attribute"
    This reverts commit 43bd91b710d6778248ee9932a33aed81d06b23a1.

--- app/Data/DTOs/TelegramUserDto.cs
+++ app/Data/DTOs/TelegramUserDto.cs
@@ -1,13 +1,11 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace app.Data.DTOs
 {
     public class TelegramUserDto
     {
         // used for verifying hash
         // naming conventions are changed to match data received from telegram
-        [Required] public long id { get; set; }
-        [Required] public string first_name { get; set; }
+        public long id { get; set; }
+        public string first_name { get; set; }
         public string username { get; set; }
         public string photo_url { get; set; }
 
